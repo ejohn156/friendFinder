e1aa8707177cc4b10c3bdbd08c21eb50f948cc72
Eric Johnson <ejohn156@uncc.edu>
2018-07-12 11:15:13 -0400

    set up htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,14 @@
+var express = require('express')
+var router = express.Router()
+var path = require("path")
+module.exports = router
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+})
+router.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+})
+
+
+
 
--- server.js
+++ server.js
@@ -4,17 +4,12 @@ var bodyParser = require("body-parser")
 var path = require("path")
 var PORT = process.env.PORT || 8080;
 var app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+var htmlRoutes = require('./app/routing/htmlRoutes')
 
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-})
-
-app.get("/survey", function(req,res){
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-})
-
+app.use("/",htmlRoutes)
 
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
