3ac6d80a5c942a49ffee2acfa6a29166de3db1be
Eric Johnson <ejohn156@uncc.edu>
2018-07-12 09:45:50 -0400

    set up package json and ready to start coding

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ var express = require("express")
 var mysql = require("mysql")
 var body = require("body-parser")
 var path = require("path")
+var PORT = process.env.PORT || 8080;
+var app = express();
