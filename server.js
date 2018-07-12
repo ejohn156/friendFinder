b94eeb7b6bc82b5b40ff33a5198554f9634535a9
Eric Johnson <ejohn156@uncc.edu>
2018-07-12 10:10:08 -0400

    set up basic routes

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require("express")
 var mysql = require("mysql")
-var body = require("body-parser")
+var bodyParser = require("body-parser")
 var path = require("path")
 var PORT = process.env.PORT || 8080;
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+})
+
+app.get("/survey", function(req,res){
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+})
+
+
+app.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:" + PORT);
+})
